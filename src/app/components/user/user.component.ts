62f6aea877bd707e82f4cec22684c33d7ef2e72f
Felix <felix.dryselius@gmail.com>
2018-03-26 14:33:49 +0200

    done 47 min of Traversy Media's tutorial of angular4. The app is now working properly.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  name:string = "Felix";
+  age:number = 55;
+  address:Address;
+  hobbies:string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.address={
+      street: "Tinderella Road",
+      city: "Gothem",
+      state:"Non Of YOUR Business",
+      zip:895,
+    };
+    this.hobbies = ["looking at cats", "trying all my furries..."];
+  }
+
+  onClick(){
+    console.log("lololololol");
+    this.hobbies.push("Olja Oh olja");
+    
+  }
+
+  addHobby(hobby){
+  console.log("addHobby() runned with value:");
+  console.log(hobby);
+  this.hobbies.unshift(hobby)
+  return false;     
+  }
+
+  deleteHobby(hobby){
+    console.log(hobby);
+    for(let i = 0; i<this.hobbies.length;i++){
+      if(this.hobbies[i]==hobby){
+        this.hobbies.splice(i,hobby.length);
+      }
+    }
+    
   }
 
 }
+
+
+interface Address{
+  street:string,
+  city:string,
+  state:string,
+  zip:number,
+}
+
